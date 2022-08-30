5d2695190f6f86810fa5179600798cbb0f50734d
yangzhuang <johnyang@yunify.com>
2022-08-30 10:32:10 +0800

    update

--- Python/20210714/my_signal.py
+++ Python/20210714/my_signal.py
@@ -1,12 +1,12 @@
 import signal
+
+
 # Define signal handler function
 def myHandler(signum, frame):
     print('I received: ', signum)
+
 
 # register signal.SIGTSTP's handler
 signal.signal(signal.SIGTSTP, myHandler)
 signal.pause()
 print('End of Signal Demo')
-# ————————————————
-# 版权声明：本文为CSDN博主「iloveyin」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-# 原文链接：https://blog.csdn.net/iloveyin/article/details/45147161
